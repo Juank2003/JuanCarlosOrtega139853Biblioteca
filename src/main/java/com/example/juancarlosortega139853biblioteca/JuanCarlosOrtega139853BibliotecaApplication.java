a5f93a72eb170b50010033778d6ae76ae4b8e998
Juank003 <jortealv@myuax.com>
2023-11-23 18:15:07 +0100

    Initial commit

---
+++ src/main/java/com/example/juancarlosortega139853biblioteca/JuanCarlosOrtega139853BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.juancarlosortega139853biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanCarlosOrtega139853BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanCarlosOrtega139853BibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juancarlosortega139853biblioteca/JuanCarlosOrtega139853BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juancarlosortega139853biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanCarlosOrtega139853BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
